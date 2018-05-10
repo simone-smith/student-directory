6cb20a1eb51af25c0dc0f09b10e2780665651915
Simone Smith <simonecaplin@gmail.com>
2018-05-10 16:16:23 +0000

    Take arguments from the command line

--- directory.rb
+++ directory.rb
@@ -1,49 +1,5 @@
 @students = [] # an empty array accessible to all methods
 
-def input_students
-  puts "Please enter the names of the students, followed by their cohort, country of
-  birth and height"
-  puts "To finish, just hit return twice"
-  # get the first name, cohort, country and height
-  puts "Name: "
-  name = gets.delete("\n").capitalize
-  puts "Cohort: "
-  cohort = gets.delete("\n").capitalize.to_sym
-  puts "Country: "
-  country = gets.delete("\n").capitalize
-  puts "Height: "
-  height = gets.delete("\n").capitalize
-  # while the name is not empty, repeat this code
-  while !name.empty? do
-    cohort = "May" if cohort.empty?
-    country = "UK" if country.empty?
-    height = "6 ft" if height.empty?
-
-    # add the student hash to the array
-    @students << {name: name, cohort: cohort, country: country, height: height}
-    puts "Now we have #{@students.count} student" if @students.count == 1
-    puts "Now we have #{@students.count} students" if @students.count > 1
-    # get another name, cohort, country and height from the user
-    puts "Enter another name, or hit return again to finish."
-    name = gets.delete("\n").capitalize
-    if !name.empty?
-      puts "Cohort: "
-      cohort = gets.delete("\n").capitalize.to_sym
-      puts "Country: "
-      country = gets.delete("\n").capitalize
-      puts "Height: "
-      height = gets.delete("\n").capitalize
-    end
-  end
-end
-
-def interactive_menu
-  loop do
-    print_menu
-    process(gets.chomp)
-  end
-end 
-
 def print_menu
   puts "1. Input the students"
   puts "2. Show the students"
@@ -52,11 +8,12 @@ def print_menu
   puts "9. Exit" # 9 because we'll be adding more items
 end
 
-def show_students
-  print_header
-  print_students_list
-  print_footer
-end
+def interactive_menu
+  loop do
+    print_menu
+    process(STDIN.gets.chomp)
+  end
+end 
 
 def process(selection)
   case selection
@@ -73,6 +30,49 @@ def process(selection)
     else
       puts "I don't know what you meant, try again"
   end
+end
+
+def input_students
+  puts "Please enter the names of the students, followed by their cohort, country of
+  birth and height"
+  puts "To finish, just hit return twice"
+  # get the first name, cohort, country and height
+  puts "Name: "
+  name = STDIN.gets.delete("\n").capitalize
+  puts "Cohort: "
+  cohort = STDIN.gets.delete("\n").capitalize.to_sym
+  puts "Country: "
+  country = STDIN.gets.delete("\n").capitalize
+  puts "Height: "
+  height = STDIN.gets.delete("\n").capitalize
+  # while the name is not empty, repeat this code
+  while !name.empty? do
+    cohort = "May" if cohort.empty?
+    country = "UK" if country.empty?
+    height = "6 ft" if height.empty?
+
+    # add the student hash to the array
+    @students << {name: name, cohort: cohort, country: country, height: height}
+    puts "Now we have #{@students.count} student" if @students.count == 1
+    puts "Now we have #{@students.count} students" if @students.count > 1
+    # get another name, cohort, country and height from the user
+    puts "Enter another name, or hit return again to finish."
+    name = STDIN.gets.delete("\n").capitalize
+    if !name.empty?
+      puts "Cohort: "
+      cohort = STDIN.gets.delete("\n").capitalize.to_sym
+      puts "Country: "
+      country = STDIN.gets.delete("\n").capitalize
+      puts "Height: "
+      height = STDIN.gets.delete("\n").capitalize
+    end
+  end
+end
+
+def show_students
+  print_header
+  print_students_list
+  print_footer
 end
 
 def print_header
@@ -122,8 +122,8 @@ def save_students
   file.close
 end
 
-def load_students
-  file = File.open("students.csv", "r")
+def load_students(filename = "students.csv")
+  file = File.open(filename, "r")
   file.readlines.each do |line|
     name, cohort, country, height = line.chomp.split(",")
     @students << {name: name, cohort: cohort.to_sym, country: country, height: height}
@@ -131,5 +131,18 @@ def load_students
   file.close
 end
 
+def try_load_students
+  filename = ARGV.first # first argument from the command line
+  return if filename.nil? # get out of the method if it isn't given
+  if File.exists?(filename)
+    load_students(filename)
+    puts "Loaded #{@students.count} from #{filename}"
+  else # if it doesn't exist
+    puts "Sorry, #{filename} doesn't exist."
+    exit # quit the program
+  end
+end
+
 # nothing happens until we call the method
+try_load_students
 interactive_menu
