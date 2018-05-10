27747016c27fed97e180cc48cce1674711028fdd
Simone Smith <simonecaplin@gmail.com>
2018-05-10 13:38:01 +0000

    return to step 8 to commit the exercises

--- directory.rb
+++ directory.rb
@@ -1,57 +1,20 @@
-@students = [] # an empty array accessible to all methods
-
-def print_menu
-  puts "1. Input the students"
-  puts "2. Show the students"
-  puts "3. Save the list to students.csv"
-  puts "4. Load the list from students.csv"
-  puts "9. Exit"
-end 
-
-def interactive_menu
-  loop do
-    print_menu
-    process(STDIN.gets.chomp)
-  end
-end
-
-def process(selection)
-  case selection
-    when "1"
-      input_students
-    when "2"
-      show_students
-    when "3"
-      save_students
-    when "4"
-      load_students
-    when "9"
-      exit
-    else
-      puts "I don't know what you meant, try again"
-  end
-end
-
 def input_students
   puts "Please enter the names of the students"
   puts "To finish, just hit return twice"
+  # create an empty array
+  students = []
   # get the first name
-  name = STDIN.gets.chomp
+  name = gets.chomp
   # while the name is not empty, repeat this code
   while !name.empty? do
     # add the student hash to the array
-    @students << {name: name, cohort: :november}
-    puts "Now we have #{@students.count} student" if @students.count == 1
-    puts "Now we have #{@students.count} students" if @students.count > 1
+    students << {name: name, cohort: :november}
+    puts "Now we have #{students.count} students"
     # get another name from the user
-    name = STDIN.gets.chomp
+    name = gets.chomp
   end
-end
-
-def show_students
-  print_header
-  print_student_list
-  print_footer
+  # return the array of students
+  students
 end
 
 def print_header
@@ -59,49 +22,18 @@ def print_header
   puts "-------------"
 end
 
-def print_student_list
-  @students.each do |student|
+def print(students)
+  students.each do |student|
     puts "#{student[:name]} (#{student[:cohort]} cohort)"
   end
-end 
-
-def print_footer
-  puts "Overall, we have #{@students.count} great student" if @students.count == 1
-  puts "Overall, we have #{@students.count} great students" if @students.count > 1
 end
 
-def save_students
-  # open the file for writing
-  file = File.open("students.csv", "w")
-  # iterate over the array of students
-  @students.each do |student|
-    student_data = [student[:name], student[:cohort]]
-    csv_line = student_data.join(",")
-    file.puts csv_line
-  end
-  file.close
+def print_footer(students)
+  puts "Overall, we have #{students.count} great students"
 end
 
-def load_students(filename = "students.csv")
-  file = File.open(filename, "r")
-  file.readlines.each do |line|
-  name, cohort = line.chomp.split(",")
-    @students << {name: name, cohort: cohort.to_sym}
-  end
-  file.close
-end
-
-def try_load_students
-  filename = ARGV.first # first argument from the command line
-  return if filename.nil? # get out of the method if it isn't given
-  if File.exists?(filename) # if it exists
-    load_students(filename)
-      puts "Loaded #{@students.count} from #{filename}"
-  else # if it doesn't exist
-    puts "Sorry, #{filename} doesn't exist."
-    exit # quit the programme
-  end
-end
-
-try_load_students
-interactive_menu
+students = input_students
+#nothing happens until we call the methods
+print_header
+print(students)
+print_footer(students)
