7303d9b66d27693c4ae65b2213d78945b8cc7cd2
Simone Smith <simonecaplin@gmail.com>
2018-05-10 13:47:53 +0000

    Step 8, exercise 2

--- directory.rb
+++ directory.rb
@@ -4,14 +4,14 @@ def input_students
   # create an empty array
   students = []
   # get the first name
-  name = gets.chomp
+  name = gets.chomp.capitalize
   # while the name is not empty, repeat this code
   while !name.empty? do
     # add the student hash to the array
     students << {name: name, cohort: :november}
     puts "Now we have #{students.count} students"
     # get another name from the user
-    name = gets.chomp
+    name = gets.chomp.capitalize
   end
   # return the array of students
   students
@@ -23,8 +23,12 @@ def print_header
 end
 
 def print(students)
+  puts "Type first letter to filter names by"
+  letter = gets.chomp.upcase
   students.each_with_index do |student, index|
-    puts "#{index + 1}. #{student[:name]} (#{student[:cohort]} cohort)"
+    if student[:name][0] == letter
+      puts "#{index + 1}. #{student[:name]} (#{student[:cohort]} cohort)"
+    end
   end
 end
 
