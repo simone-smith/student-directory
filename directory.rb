983da97c369fc12f2385893602401f5cb030ee9b
Simone Smith <simonecaplin@gmail.com>
2018-05-10 15:52:18 +0000

    More refactoring

--- directory.rb
+++ directory.rb
@@ -1,27 +1,36 @@
+@students = [] # an empty array accessible to all methods
+
 def interactive_menu
-  students = []
   loop do
-    # 1. print the menu and ask the user what to do
-    puts "1. Input the students"
-    puts "2. Show the students"
-    puts "9. Exit" # 9 because we'll be adding more items
-    # 2. read the input and save it into a variable
-    selection = gets.chomp
-    # 3. do what the user has asked
-    case selection
-    when "1"
-      students = input_students
-    when "2"
-      print_header(students)
-      print(students)
-      print_footer(students)
-    when "9"
-      exit # this will cause the program to terminate
-    else
-      puts "I don't know what you meant, try again"
-    end
+    print_menu
+    process(gets.chomp)
   end
 end 
+
+def process(selection)
+  case selection
+    when "1"
+      input_students
+    when "2"
+      show_students
+    when "9"
+      exit
+    else
+      puts "I don't know what you meant, try again"
+  end
+end
+
+def print_menu
+  puts "1. Input the students"
+  puts "2. Show the students"
+  puts "9. Exit" # 9 because we'll be adding more items
+end
+
+def show_students
+  print_header
+  print_students_list
+  print_footer
+end
 
 def input_students
   puts "Please enter the names of the students, followed by their cohort, country of
@@ -45,9 +54,9 @@ def input_students
     height = "6 ft" if height.empty?
 
     # add the student hash to the array
-    students << {name: name, cohort: cohort, country: country, height: height}
-    puts "Now we have #{students.count} student" if students.count == 1
-    puts "Now we have #{students.count} students" if students.count > 1
+    @students << {name: name, cohort: cohort, country: country, height: height}
+    puts "Now we have #{@students.count} student" if @students.count == 1
+    puts "Now we have #{@students.count} students" if @students.count > 1
     # get another name, cohort, country and height from the user
     puts "Enter another name, or hit return again to finish."
     name = gets.delete("\n").capitalize
@@ -60,29 +69,27 @@ def input_students
       height = gets.delete("\n").capitalize
     end
   end
-  # return the array of students
-  students
 end
 
-def print_header(students)
-  if students.count > 0
+def print_header
+  if @students.count > 0
     puts "The students of Villains Academy".center(50)
     puts "-------------".center(50)
   end
 end
 
-def print(students)
+def print_students_list
   # print the students grouped by cohort
   cohort_array = []
   
-  students.each do |hash|
+  @students.each do |hash|
     cohort_array.push(hash[:cohort]).uniq!
   end
   
   cohort_array.each do |month|
     puts month.to_s.center(50)
-    students.each_with_index do |student, index|
-      if students.count > 0
+    @students.each_with_index do |student, index|
+      if @students.count > 0
         if student[:cohort] == month
           puts "#{index + 1}. #{student[:name]} (#{student[:cohort]} cohort)".center(50)
         end 
@@ -91,11 +98,13 @@ def print(students)
   end
 end
 
-def print_footer(students)
-  if students.count > 0
-    puts "Overall, we have #{students.count} great students".center(50)
+def print_footer
+  if @students.count == 1
+    puts "Overall, we have #{@students.count} great student.".center(50) 
+  else 
+    puts "Overall, we have #{@students.count} great students".center(50)
   end
 end
 
-#nothing happens until we call the method
+# nothing happens until we call the method
 interactive_menu
